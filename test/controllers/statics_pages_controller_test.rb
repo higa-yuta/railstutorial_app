4bcc459fbb23ba3530ceba833fa7e803edd33577
yutahiga <h.yuta2314@gmail.com>
2022-03-01 20:12:04 +0000

    Modified static_pages_controller_test.rb: Add test of title

--- test/controllers/statics_pages_controller_test.rb
+++ test/controllers/statics_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticsPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end

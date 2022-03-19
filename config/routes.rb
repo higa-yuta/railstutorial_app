5f7b41ee4a0309ba93de75314da758ef77f08d9b
yuta higa <h.yuta2314@gmail.com>
2022-03-19 14:07:07 +0000

    Add signup_path to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'statics_pages#home'
 
   get '/home', to: 'statics_pages#home'
   get '/help', to: 'statics_pages#help'
   get '/about', to: 'statics_pages#about'
   get '/contact', to: 'statics_pages#contact'
+
+  get '/signup', to: 'users/#new'
 
 end

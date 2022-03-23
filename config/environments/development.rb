b15acceb65f649b061454cc7653a5581af1d0a57
yuta higa <h.yuta2314@gmail.com>
2022-03-23 09:40:43 +0000

    Set up ip address allocation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Set up asset allocation
+  # Check ip address(Vagrantfile)
+  config.action_controller.asset_host = "192.168.50.4"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 

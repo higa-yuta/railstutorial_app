fe3b0f7e00756c45b56abe52a880cad52a2f4d88
yuta higa <h.yuta2314@gmail.com>
2022-05-04 09:47:05 +0000

    Modified session-related files and login test.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = "You input information is Invalid."
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
